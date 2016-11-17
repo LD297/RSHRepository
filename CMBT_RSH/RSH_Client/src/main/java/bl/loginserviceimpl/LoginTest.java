cd45a253731febfaede643631c012092fb7055e9
NjuWxy <151250148@smail.nju.edu.cn>
2016-11-17 21:57:04 +0800

    导入项目

---
+++ CMBT_RSH/RSH_Client/src/main/java/bl/loginserviceimpl/LoginTest.java
@@ -0,0 +1,20 @@
+package bl.loginserviceimpl;
+
+
+
+import constant.ResultMessage;
+import constant.Role;
+
+public class LoginTest {
+	@Test
+	public void testCheckOnline(){
+		ResultMessage resultMessage1 = Login.checkOnline(Role.user, "13951897687","jksggkskjg" );
+		assertEquals(ResultMessage.success,resultMessage1);
+		ResultMessage resultMessage2 = Login.checkOnline(Role.hotel, "13951897687","jksggkskjg" );
+		assertEquals(ResultMessage.success,resultMessage2);
+		ResultMessage resultMessage3 = Login.checkOnline(Role.websalesman, "13951897687","jksggkskjg" );
+		assertEquals(ResultMessage.success,resultMessage3);
+		ResultMessage resultMessage4 = Login.checkOnline(Role.webmanager, "13951897687","jksggkskjg" );
+		assertEquals(ResultMessage.success,resultMessage4);
+	}
+}
