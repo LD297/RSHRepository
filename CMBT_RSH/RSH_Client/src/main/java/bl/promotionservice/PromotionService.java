5774c42a59472422d2be0bd70104fd85b26986f7
NjuWxy <151250148@smail.nju.edu.cn>
2016-11-18 23:51:34 +0800

    为promotionservice建包

--- CMBT_RSH/RSH_Client/src/main/java/bl/promotionservice/PromotionService.java
+++ CMBT_RSH/RSH_Client/src/main/java/bl/promotionservice/PromotionService.java
@@ -1,28 +1,22 @@
-package bl;
+package bl.promotionservice;
+
+import constant.*;
+import po.OrderPO;
+import vo.PromotionVO;
 
 import java.util.ArrayList;
 import java.util.Date;
-
-import javax.naming.spi.DirStateFactory.Result;
-
-import constant.ConditionType;
-import constant.PromotionType;
-import constant.ResultMessage;
-import constant.RoomType;
-import constant.ScopeType;
-import po.OrderPO;
-import vo.PromotionVO;
 
 public interface PromotionService {
 
 	/*
 	 * 
 	 */
-	public ResultMessage addPromotion(String reason,String ID);
+	public ResultMessage addPromotion(String reason, String ID);
 	
-	public ResultMessage setScope(ScopeType type,String id, RoomType type2);
+	public ResultMessage setScope(ScopeType type, String id, RoomType type2);
 	
-	public ResultMessage setConditioniType(ConditionType type,int Requirement);
+	public ResultMessage setConditioniType(ConditionType type, int Requirement);
 	
 	public ResultMessage setPromotionType(PromotionType type, int num);
 	
@@ -30,15 +24,15 @@ public interface PromotionService {
 	
 	public ResultMessage update();
 	
-	public ResultMessage delPromotion(String reason,String ID);
+	public ResultMessage delPromotion(String reason, String ID);
 	
 	public ArrayList<PromotionVO> getPromotionOfPeriod(Date beginDate, Date EndDate);
 	
-	public ArrayList <PromotionVO> getPromotionOfRoom (String hotelID, RoomType type);
+	public ArrayList <PromotionVO> getPromotionOfRoom(String hotelID, RoomType type);
 	
 	public ArrayList<PromotionVO> getPromotionOfHotel(String hotelID);
 	
-	public ArrayList <PromotionVO> getPromotionOfDistrict (String district);
+	public ArrayList <PromotionVO> getPromotionOfDistrict(String district);
 	
 	public String countPromotionOfRoom(String hotelID, RoomType type, int num, Date beginDate, Date endDate);
 	
