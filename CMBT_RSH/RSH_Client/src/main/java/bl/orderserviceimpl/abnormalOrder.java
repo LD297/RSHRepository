9097ed36156c8b2611ddb2bdee1b2910488f42cf
NjuWxy <151250148@smail.nju.edu.cn>
2016-11-27 20:02:22 +0800

    该类名

--- CMBT_RSH/RSH_Client/src/main/java/bl/orderserviceimpl/abnormalOrder.java
+++ CMBT_RSH/RSH_Client/src/main/java/bl/orderserviceimpl/abnormalOrder.java
@@ -1,64 +1,66 @@
 package bl.orderserviceimpl;
 
-import java.util.Date;
-import java.util.ArrayList;
-import java.text.SimpleDateFormat;
 import bl.orderservice.HotelInfoService;
 import bl.userserviceimpl.CreditRecordList;
-import constant.*;
+import constant.ResultMessage;
 import po.OrderPO;
 import vo.RoomNormVO;
-import vo.CreditRecordVO;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by john on 2016/11/27.
+ */
 public class AbnormalOrder {
 
-	HotelInfoService hotelinfo;
-	CreditRecordList record;
-	OrderPO orderpo;
+    HotelInfoService hotelinfo;
+    CreditRecordList record;
+    OrderPO orderpo;
 
-	public void setCreditRecordList(CreditRecordList re){
-		record = re;
-	}
-	public void setOrder(OrderPO order){
-		orderpo = order;
-	}
+    public void setCreditRecordList(CreditRecordList re){
+        record = re;
+    }
+    public void setOrder(OrderPO order){
+        orderpo = order;
+    }
 
 
-	// 酒店手动补登记 改变订单状态 信用值
-	public ResultMessage hotelCancelAbnormal(String orderid){
-		//dataservice update
-		//dataservice find
-		//CreditRecordList
-		return null;
-	}
+    // 酒店手动补登记 改变订单状态 信用值
+    public ResultMessage hotelCancelAbnormal(String orderid){
+        //dataservice update
+        //dataservice find
+        //CreditRecordList
+        return null;
+    }
 
-	// 网站营销人员查询异常订单
-	public ArrayList<OrderPO> browseAbnormal(){
-		//dataservice statefind
-		return null;
-	}
+    // 网站营销人员查询异常订单
+    public ArrayList<OrderPO> browseAbnormal(){
+        //dataservice statefind
+        return null;
+    }
 
-	//
-	public void handleAbnormalRoom(){
-		ArrayList<OrderPO> abList = this.browseAbnormal();
-		for(int i=0;i<abList.size();i++){
+    //
+    public void handleAbnormalRoom(){
+        ArrayList<OrderPO> abList = this.browseAbnormal();
+        for(int i=0;i<abList.size();i++){
 
-			ArrayList<RoomNormVO> rooms = abList.get(i).getRooms();
-			int size = rooms.size();
-			int nums[] = new int[size];
-			nums = abList.get(i).getRoomNums();
-			Date checkIn = abList.get(i).getInTime()[0];
-			Date checkOut = abList.get(i).getInTime()[1];
+            ArrayList<RoomNormVO> rooms = abList.get(i).getRooms();
+            int size = rooms.size();
+            int nums[] = new int[size];
+            nums = abList.get(i).getRoomNums();
+            Date checkIn = abList.get(i).getInTime()[0];
+            Date checkOut = abList.get(i).getInTime()[1];
 
-			for(int j=0;j<size;j++)//rooms.size
-				hotelinfo.changeRoomAvail(rooms.get(j).roomType,nums[j] , checkIn, checkOut);
-		}
-	}
+            for(int j=0;j<size;j++)//rooms.size
+                hotelinfo.changeRoomAvail(rooms.get(j).roomType,nums[j] , checkIn, checkOut);
+        }
+    }
 
-	// 网站营销人员撤销异常订单
-	public ResultMessage webCancelAbnormal(String orderid){//cause:申诉->change credit
+    // 网站营销人员撤销异常订单
+    public ResultMessage webCancelAbnormal(String orderid){//cause:申诉->change credit
 
-		//orderdataservice->findorderpo
+        //orderdataservice->findorderpo
 /*		ArrayList<RoomNormVO> rooms =  orderpo.norm;
 		int num[] = new int[rooms.size()];
 		num = orderpo.numbers;
@@ -67,12 +69,13 @@ public class AbnormalOrder {
 
 		for(int i=0;i<rooms.size();i++)
 		    hotelinfo.changeRoomAvail(rooms.get(i).type, num[i],checkIn,checkOut);
-	
+
 		//userid,date,orderid,creditAction,change,credit
 		Date date = new Date();
 		CreditRecordVO creditRecord = new CreditRecordVO("", date, orderid,null, "", 0);
 		record.addCreditRecord(creditRecord);*/
-		return ResultMessage.succeed;
-	}
+        return ResultMessage.succeed;
+    }
 
 }
+
--- CMBT_RSH/RSH_Client/src/main/java/bl/orderserviceimpl/normalOrderTest.java
+++ CMBT_RSH/RSH_Client/src/main/java/bl/orderserviceimpl/normalOrderTest.java
@@ -1,46 +1,49 @@
 package bl.orderserviceimpl;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 import bl.userserviceimpl.CreditRecordList;
 import constant.ResultMessage;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by john on 2016/11/27.
+ */
 public class NormalOrderTest {
-	@Test
-	public void testComment(){
+    @Test
+    public void testComment(){
         MockComment mockComment = new MockComment("2153001234","123456789");
         NormalOrder normal= new NormalOrder();
-        
+
         normal.setCommentImpl(mockComment);
         assertEquals(ResultMessage.fail,normal.comment("2153001234","2016-11-062153001234000000",80,""));
     }
- /*   @Test
-	public void testcancelMyOrder(){
-		MockCreditRecordList mockCredit = new MockCreditRecordList("123456789");
-		normalOrder normal = new normalOrder();
-		normal.setCreditRecordList(mockCredit);
-		
-		normal.cancelMyOrder("2016-11-072153001234000000");
-	}
-*/
-	@Test
-	public void testexecute(){
-		CreditRecordList mockCredit = new CreditRecordList("123456789");
-		NormalOrder normal = new NormalOrder();
-		normal.setCreditRecordList(mockCredit);
-		
-		assertEquals(ResultMessage.succeed,normal.execute("2016-11-072153001234000000"));
-		   
-	}
+    /*   @Test
+       public void testcancelMyOrder(){
+           MockCreditRecordList mockCredit = new MockCreditRecordList("123456789");
+           normalOrder normal = new normalOrder();
+           normal.setCreditRecordList(mockCredit);
+
+           normal.cancelMyOrder("2016-11-072153001234000000");
+       }
+   */
+    @Test
+    public void testexecute(){
+        CreditRecordList mockCredit = new CreditRecordList("123456789");
+        NormalOrder normal = new NormalOrder();
+        normal.setCreditRecordList(mockCredit);
+
+        assertEquals(ResultMessage.succeed,normal.execute("2016-11-072153001234000000"));
+
+    }
 /*  @Test
 	public void testsetAbnormal(String orderid){
 		MockCreditRecordList mockCredit = new MockCreditRecordList("123456789");
 		normalOrder normal = new normalOrder();
 		normal.setCreditRecordList(mockCredit);
-		
+
 		normal.setAbnormal("2016-11-072153001234000000");
 	}
 */
 }
+
