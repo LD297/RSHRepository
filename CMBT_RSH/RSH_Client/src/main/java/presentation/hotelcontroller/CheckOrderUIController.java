3084453cbc140fa196b70836786c921035e769be
LD297 <875928078@qq.com>
2016-12-08 22:49:28 +0800

    orderpo

--- CMBT_RSH/RSH_Client/src/main/java/presentation/hotelcontroller/CheckOrderUIController.java
+++ CMBT_RSH/RSH_Client/src/main/java/presentation/hotelcontroller/CheckOrderUIController.java
@@ -3,7 +3,13 @@ package presentation.hotelcontroller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -17,12 +23,94 @@ public class CheckOrderUIController {
     private URL location;
 
     @FXML
+    private Label userLabel;
+
+    @FXML
+    private Label expectedCheckinTimeLabel;
+
+    @FXML
+    private Button executeButton;
+
+    @FXML
+    private TextField actualCheckoutTimeTextField;
+
+    @FXML
+    private Label expectedCheckoutTimeLabel;
+
+    @FXML
+    private AnchorPane unexecutedPane2;
+
+    @FXML
+    private AnchorPane unexecutedPane0;
+
+    @FXML
+    private DatePicker actualCheckinDate;
+
+    @FXML
+    private Label generationDateLabel;
+
+    @FXML
+    private AnchorPane unexecuredPane4;
+
+    @FXML
+    private Tab exceptionalTab;
+
+    @FXML
     private AnchorPane anchorPane;
+
+    @FXML
+    private Label prePageLabel;
+
+    @FXML
+    private Tab unexecutedTab;
+
+    @FXML
+    private TextField actualCheckinTimeTextField;
+
+    @FXML
+    private DatePicker actualCheckoutDate;
 
     @FXML
     private Button backButton;
 
+    @FXML
+    private Label nextPageLabel;
+
+    @FXML
+    private Label expectedCheckinDateLabel;
+
+    @FXML
+    private Tab executedTab;
+
+    @FXML
+    private Tab revokedTab;
+
+    @FXML
+    private Label expectedCheckoutDateLabel;
+
+    @FXML
+    private Label valueLabel;
+
+    @FXML
+    private TabPane tabPane;
+
     private AnchorPane prePane;
+
+//    private ArrayList<OrderPO>;
+
+    @FXML
+    void executeButtonClicked(MouseEvent event) {
+    }
+
+    @FXML
+    void editActualCheckinTime(MouseEvent event) {
+
+    }
+
+    @FXML
+    void editActualCheckoutTime(MouseEvent event) {
+
+    }
 
     @FXML
     void backButtonClicked(MouseEvent event) {
@@ -31,17 +119,38 @@ public class CheckOrderUIController {
 
     @FXML
     void initialize() {
+        assert userLabel != null : "fx:id=\"userLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert expectedCheckinTimeLabel != null : "fx:id=\"expectedCheckinTimeLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert executeButton != null : "fx:id=\"executeButton\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert actualCheckoutTimeTextField != null : "fx:id=\"actualCheckoutTimeTextField\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert expectedCheckoutTimeLabel != null : "fx:id=\"expectedCheckoutTimeLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert unexecutedPane2 != null : "fx:id=\"unexecutedPane2\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert unexecutedPane0 != null : "fx:id=\"unexecutedPane0\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert actualCheckinDate != null : "fx:id=\"actualCheckinDate\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert generationDateLabel != null : "fx:id=\"generationDateLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert unexecuredPane4 != null : "fx:id=\"unexecuredPane4\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert exceptionalTab != null : "fx:id=\"exceptionalTab\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
         assert anchorPane != null : "fx:id=\"anchorPane\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert prePageLabel != null : "fx:id=\"prePageLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert unexecutedTab != null : "fx:id=\"unexecutedTab\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert actualCheckinTimeTextField != null : "fx:id=\"actualCheckinTimeTextField\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert actualCheckoutDate != null : "fx:id=\"actualCheckoutDate\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
         assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert nextPageLabel != null : "fx:id=\"nextPageLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert expectedCheckinDateLabel != null : "fx:id=\"expectedCheckinDateLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert executedTab != null : "fx:id=\"executedTab\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert revokedTab != null : "fx:id=\"revokedTab\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert expectedCheckoutDateLabel != null : "fx:id=\"expectedCheckoutDateLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert valueLabel != null : "fx:id=\"valueLabel\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
+        assert tabPane != null : "fx:id=\"tabPane\" was not injected: check your FXML file '订单搜索并浏览（酒店）.fxml'.";
 
+    }
+
+    public void setPrePane(AnchorPane prePane){
+        this.prePane = prePane;
     }
 
     public void setAnchorPane(AnchorPane anchorPane) {
         this.anchorPane = anchorPane;
     }
-
-    public void setPrePane(AnchorPane prePane) {
-        this.prePane = prePane;
-    }
 }
-
--- CMBT_RSH/RSH_Server/src/main/java/po/OrderPO.java
+++ CMBT_RSH/RSH_Server/src/main/java/po/OrderPO.java
@@ -1,15 +1,16 @@
 package po;
 
-import java.io.Serializable;
-import java.util.Date;
-import java.util.ArrayList;
-
+import constant.StateOfOrder;
 import vo.RoomNormVO;
-import constant.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class OrderPO implements Serializable{
 	private String orderid = null;
 	private String userid = null;
+	private String userName = null;
 	private String hotelid = null;
 	private StateOfOrder state = null;
 	private ArrayList<RoomNormVO> norm = null;
@@ -20,8 +21,27 @@ public class OrderPO implements Serializable{
 	private String promotion =  null;
 	private String comment = null;
 	private int grade = 0;
+	/**
+	 * 预计入住日期（含时间）（用户操作）
+	 */
 	private Date checkIn = null;
+	/**
+	 * 预计离开日期（含时间）（用户操作）
+	 */
 	private Date checkOut = null;
+	/**
+	 * 实际入住日期（含时间）（酒店操作）
+	 */
+	private Date actualCheckIn = null;
+	/**
+	 * 实际离开日期（含时间）（酒店操作）
+	 */
+	private Date actualCheckOut = null;
+	/**
+	 * 订单生成日期（界面暂时不显示时间，但可以保存以备需求变更）
+	 */
+	private Date generationDate = null;
+
 	private int numOfPeople = 0;
 	private boolean adultOnly = false;
 
@@ -73,7 +93,17 @@ public class OrderPO implements Serializable{
 		state = condition;
 	}
 
-	public OrderPO(String order,String hotel,StateOfOrder s,double origin,double discounted,Date in,Date out){
+	/**
+	 *
+	 * @param order
+	 * @param hotel
+	 * @param s
+	 * @param origin
+	 * @param discounted
+	 * @param in
+	 * @param out
+	 */
+	public OrderPO(String order, String hotel, StateOfOrder s, double origin, double discounted, Date in, Date out){
 		orderid = order;
 		hotelid = hotel;
 		originvalue = origin;
@@ -82,18 +112,42 @@ public class OrderPO implements Serializable{
 		checkIn = in;
 		checkOut = out;
 	}
-	public OrderPO(String order,String user,StateOfOrder s,double origin,double discounted,Date in,Date out,boolean adultonly,int peoplenum){
+
+	/**
+     * 酒店视角
+	 *
+	 * @param order
+     * @param userI
+     * @param userN
+     * @param s
+     * @param origin
+     * @param discounted
+     * @param in
+     * @param out
+     * @param actualIn
+     * @param actualOut
+     * @param generationDate
+     * @param adultonly
+     * @param peoplenum
+	 */
+	public OrderPO(String order, String userI, String userN, StateOfOrder s, double origin, double discounted,
+                   Date in, Date out, Date actualIn, Date actualOut, Date generationDate,
+                   boolean adultonly, int peoplenum){
 		orderid = order;
-		userid = user;
+		userid = userI;
+		userName = userN;
 		originvalue = origin;
 		truevalue = discounted;
 		state = s;
 		checkIn = in;
 		checkOut = out;
+		actualCheckIn = actualIn;
+		actualCheckOut = actualOut;
+		this.generationDate = generationDate;
 		adultOnly = adultonly;
 		numOfPeople = peoplenum;
 	}
-	public OrderPO(String order,String user,String hotel,StateOfOrder s,double discounted,Date in,Date out){
+	public OrderPO(String order, String user, String hotel, StateOfOrder s, double discounted, Date in, Date out){
 		orderid = order;
 		hotelid = hotel;
 		userid = user;
@@ -102,10 +156,30 @@ public class OrderPO implements Serializable{
 		checkIn = in;
 		checkOut = out;
 	}
-	public OrderPO(String order,String user,String hotel,StateOfOrder s,
-				   ArrayList<RoomNormVO> type,double[] roomprices,int[] nums,int people,boolean adultonly,
-				   double origin,double discounted, String pro,
-				   String com,int gra,Date in,Date out){
+
+	/**
+	 *
+	 * @param order
+	 * @param user
+	 * @param hotel
+	 * @param s
+	 * @param type
+	 * @param roomprices
+	 * @param nums
+	 * @param people
+	 * @param adultonly
+	 * @param origin
+	 * @param discounted
+	 * @param pro
+	 * @param com
+	 * @param gra
+	 * @param in
+	 * @param out
+	 */
+	public OrderPO(String order, String user, String hotel, StateOfOrder s,
+                   ArrayList<RoomNormVO> type, double[] roomprices, int[] nums, int people, boolean adultonly,
+                   double origin, double discounted, String pro,
+                   String com, int gra, Date in, Date out){
 
 		orderid = order;
 		userid = user;
