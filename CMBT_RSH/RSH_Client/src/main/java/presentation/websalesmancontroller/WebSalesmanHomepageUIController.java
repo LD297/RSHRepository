1611bc1715c4fbe729b188c25af3962865b804e0
LD297 <875928078@qq.com>
2016-12-18 23:25:37 +0800

    controller

--- CMBT_RSH/RSH_Client/src/main/java/presentation/websalesmancontroller/WebSalesmanHomepageUIController.java
+++ CMBT_RSH/RSH_Client/src/main/java/presentation/websalesmancontroller/WebSalesmanHomepageUIController.java
@@ -27,7 +27,7 @@ public class WebSalesmanHomepageUIController {
     private URL location;
 
     @FXML
-    private ImageView aboutUs;
+    private ImageView makeMememberStandard;
 
     @FXML
     private ImageView promotin;
@@ -50,12 +50,12 @@ public class WebSalesmanHomepageUIController {
     private static AnchorPane promotionPane;
     private static AnchorPane exceptionalOrderPane;
     private static AnchorPane topUpCreditPane;
-    private static AnchorPane aboutUsPane;
+    private static AnchorPane makeMemberStandardPane;
 
     private static PromotionUIController promotionUIController;
     private static ExceptionalOrderUIController exceptionalOrderUIController;
     private static TopUpCreditUIController topUpCreditUIController;
-    private static AboutUsUIController aboutUsUIController;
+    private static AboutUsUIController makeMemberStandardUIController;
 
     private static final int NUM_OF_NEXTPAGE = 4;
 
@@ -63,7 +63,7 @@ public class WebSalesmanHomepageUIController {
         FXMLLoader loader0 = HotelAndWebSalesmanUIFactory.getInstance().getPromotionUILoader();
         FXMLLoader loader1 = HotelAndWebSalesmanUIFactory.getInstance().getExceptionalOrderUILoader();
         FXMLLoader loader2 = HotelAndWebSalesmanUIFactory.getInstance().getTopUpCreditUILoader();
-        FXMLLoader loader3 = HotelAndWebSalesmanUIFactory.getInstance().getAboutUsUILoader();
+        FXMLLoader loader3 = HotelAndWebSalesmanUIFactory.getInstance().getMakeMemberStandardUILoader();
 
         // 加载各页根结点
         if(promotionPane==null)
@@ -84,9 +84,9 @@ public class WebSalesmanHomepageUIController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        if(aboutUsPane==null)
+        if(makeMemberStandardPane==null)
             try {
-                aboutUsPane = (AnchorPane) loader3.load();
+                makeMemberStandardPane = (AnchorPane) loader3.load();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -98,14 +98,14 @@ public class WebSalesmanHomepageUIController {
             exceptionalOrderUIController = loader1.getController();
         if(topUpCreditUIController==null)
             topUpCreditUIController = loader2.getController();
-        if(aboutUsUIController==null)
-            aboutUsUIController = loader3.getController();
+        if(makeMemberStandardUIController==null)
+            makeMemberStandardUIController = loader3.getController();
 
         // 传入首页根结点
         promotionUIController.setPrePane(anchorPane);
         exceptionalOrderUIController.setPrePane(anchorPane);
         topUpCreditUIController.setPrePane(anchorPane);
-        aboutUsUIController.setPrePane(anchorPane);
+        makeMemberStandardUIController.setPrePane(anchorPane);
     }
 
     @FXML
@@ -115,18 +115,6 @@ public class WebSalesmanHomepageUIController {
             scene = new Scene(promotionPane, HotelAndWebSalesmanUIFactory.UI_WIDTH, HotelAndWebSalesmanUIFactory.UI_HEIGHT);
         else
             scene = promotionPane.getScene();
-
-        Stage stage = (Stage) anchorPane.getScene().getWindow();
-        stage.setScene(scene);
-    }
-
-    @FXML
-    void changeToAboutUsUI(MouseEvent event) {
-        Scene scene = null;
-        if(aboutUsPane.getScene()==null)
-            scene = new Scene(aboutUsPane, HotelAndWebSalesmanUIFactory.UI_WIDTH, HotelAndWebSalesmanUIFactory.UI_HEIGHT);
-        else
-            scene = aboutUsPane.getScene();
 
         Stage stage = (Stage) anchorPane.getScene().getWindow();
         stage.setScene(scene);
@@ -159,8 +147,22 @@ public class WebSalesmanHomepageUIController {
     }
 
     @FXML
+    void changeToMakeMemberStandardUI(MouseEvent event) {
+        Scene scene = null;
+        if(makeMemberStandardPane.getScene()==null){
+            scene = new Scene(makeMemberStandardPane, 600, 450);
+        }
+
+        else
+            scene = makeMemberStandardPane.getScene();
+
+        Stage stage = (Stage) anchorPane.getScene().getWindow();
+        stage.setScene(scene);
+    }
+
+    @FXML
     void initialize() {
-        assert aboutUs != null : "fx:id=\"aboutUs\" was not injected: check your FXML file '网站营销人员首页.fxml'.";
+        assert makeMemberStandardPane != null : "fx:id=\"makeMemberStandardPane\" was not injected: check your FXML file '网站营销人员首页.fxml'.";
         assert promotin != null : "fx:id=\"promotin\" was not injected: check your FXML file '网站营销人员首页.fxml'.";
         assert anchorPane != null : "fx:id=\"anchorPane\" was not injected: check your FXML file '网站营销人员首页.fxml'.";
         assert logo != null : "fx:id=\"logo\" was not injected: check your FXML file '网站营销人员首页.fxml'.";
