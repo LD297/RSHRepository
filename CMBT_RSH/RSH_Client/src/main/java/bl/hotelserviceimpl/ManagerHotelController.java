f1e3f0450e7940e45e2ca2f97f6da8daa3ec6deb
AnnaYue <1023672662@qq.com>
2017-01-01 18:57:35 +0800

    add

---
+++ CMBT_RSH/RSH_Client/src/main/java/bl/hotelserviceimpl/ManagerHotelController.java
@@ -0,0 +1,38 @@
+package bl.hotelserviceimpl;
+
+import java.rmi.RemoteException;
+
+import bl.hotelservice.ManagerHotelService;
+import constant.ResultMessage;
+import data.dao.hoteldao.HotelDao;
+import rmi.RemoteHelper;
+import vo.HotelStaffVO;
+import vo.HotelVO;
+
+public class ManagerHotelController implements ManagerHotelService{
+
+	/**
+	 *  网站管理人员注册酒店账号时，根据地址（得到数量－>） 得到账号
+	 */
+	@Override
+	public String getHotelID(String district){
+		String hotelID = null;
+		RemoteHelper remoteHelper = RemoteHelper.getInstance();
+		HotelDao hotelDao = remoteHelper.getHotelDao();
+		try {
+			hotelID = hotelDao.getNewHotelID(district);
+		}catch (RemoteException e){
+			e.printStackTrace();
+			return "remote_fail";
+		}
+		return hotelID;
+	}
+
+	/**
+	 * 网站管理人员注册酒店账号时，（初始化密码后，点击确认）注册成功，生成酒店
+	 */
+	@Override
+	public ResultMessage addHotel(HotelVO hotelVO){
+		return Hotel.addHotel(hotelVO);
+	}
+}
---
+++ CMBT_RSH/RSH_Client/src/main/java/bl/userserviceimpl/UserForHotelController.java
@@ -0,0 +1,5 @@
+package bl.userserviceimpl;
+
+public class UserForHotelController {
+
+}
